58ac46ce1b411c7cecb5dbf2fbf67fa6a62bfb5a
Seun Oshitade <seun.oshitade@gmail.com>
2022-01-04 21:50:45 +0000

    Create EJS template

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const port = 3000;
 
 const app = express();
 
-app.get('/', (req, res){
-  res.send("Hello!");
-})
+app.set('view engine', 'ejs');
 
-app.listen(port, ()=> {
+app.get('/', (req, res) => {
+
+  const today = new Date();
+  const currentDay = today.getDay();
+  let day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+    console.log("Error: Current day is equal to: " + currentDay);
+  }
+  
+  res.render("list", {
+    kindOfDay: day
+  });
+
+});
+
+app.listen(port, () => {
   console.log("Server is running on port 3000!");
 })
-
