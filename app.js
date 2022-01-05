4063a0236513987e6a037f2f3b9e5b3af5459caf
Seun Oshitade <seun.oshitade@gmail.com>
2022-01-05 15:13:18 +0000

    Add ejs template and paths

--- app.js
+++ app.js
@@ -6,43 +6,31 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
 
   const today = new Date();
-  const currentDay = today.getDay();
-  let day = "";
-
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-    console.log("Error: Current day is equal to: " + currentDay);
+  
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
   }
   
+  const day = today.toLocaleDateString("en-US", options);
+
+
   res.render("list", {
     kindOfDay: day
   });
 
 });
+
+app.post('/', (req, res)=> {
+  let list = req.body.newItem;
+  console.log(list);
+})
 
 app.listen(port, () => {
   console.log("Server is running on port 3000!");
