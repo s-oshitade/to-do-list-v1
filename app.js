e4adab258eae2436b153a374acf25fa2a593a54f
Seun Oshitade <seun.oshitade@gmail.com>
2022-01-06 15:02:28 +0000

    modularise and export funtions, lint files

--- app.js
+++ app.js
@@ -1,32 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const port = 3000;
+const date = require(__dirname + "/date.js");
+
 
 const app = express();
 
-let items = ["Code everyday", "Exercise before shower", "Give thanks"];
-let workItems = [];
+const items = ["Code everyday", "Exercise before shower", "Give thanks"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
   extended: true
-}))
+}));
 
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
 
-  const today = new Date();
-
-  const options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  }
-
-  const day = today.toLocaleDateString("en-US", options);
-
+  const day = date.getDate();
 
   res.render("list", {
     listTitle: day,
@@ -37,23 +30,23 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   let item = req.body.newItem;
-  if(req.body.list === "Work List"){
-      workItems.push(item);
-      res.redirect("/work")
+  if (req.body.list === "Work List") {
+    workItems.push(item);
+    res.redirect("/work");
   } else {
-   items.push(item);
-   res.redirect("/");   
+    items.push(item);
+    res.redirect("/");
   }
-})
+});
 
 app.get("/work", (req, res) => {
-  res.render("list", {listTitle: "Work List", newListItems: workItems})
-})
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
 
 app.get("/about", (req, res) => {
   res.render("about");
-})
+});
 
 app.listen(port, () => {
   console.log("Server is running on port 3000!");
-})
+});
